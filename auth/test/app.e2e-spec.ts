d4a2edb6ea288ac5d6750d244a4e2effed6da452
yosi ziv <yosizivjs@gmail.com>
2021-01-06 20:49:31 +0200

    create e2e test

--- auth/test/app.e2e-spec.ts
+++ auth/test/app.e2e-spec.ts
@@ -1,24 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
+import { CreateUserDto } from 'src/user/dto';
+const app = 'http://localhost:3000';
+describe('ROOT', () => {
+  it('/auth (GET)', () => {
+    return request(app).get('/auth').expect(200).expect('hello world');
+  });
+});
+describe('AUTH', () => {
+  it('should create user', () => {
+    const data: CreateUserDto = {
+      user_name: 'test',
+      email: 'testt@test.com',
+      password: 'password',
+    };
+    return request(app)
+      .post('/auth/users')
+      .set('Accept', 'application/json')
+      .send(data)
+      .expect(({ body }) => {
+        expect(body.token).toBeDefined();
+        expect(body.user_name).toEqual('testt@test.com');
+        expect(body.password).toBeDefined();
+      })
+      .expect(HttpStatus.CREATED);
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('reject duplicate email on user create', () => {
+    const data: CreateUserDto = {
+      user_name: 'test',
+      email: 'testt@test.com',
+      password: 'password',
+    };
+    return request(app)
+      .post('/auth/users')
+      .set('Accept', 'application/json')
+      .send(data)
+      .expect(({ body }) => {
+        expect(body.message).toEqual('User already exsist');
+      })
+      .expect(HttpStatus.CREATED);
   });
 });
